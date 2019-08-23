33ba9ecad54590bce9ae9936d8b23efbc70fa00a
sawan4265 <54359989+sawan4265@users.noreply.github.com>
2019-08-23 14:33:24 +0530

    Add files via upload

--- testbean.js
+++ testbean.js
@@ -4,7 +4,7 @@ var httpProxy = require('http-proxy');
 var apiProxy = httpProxy.createProxyServer();
 var serverOne = 'http://220.227.2.106:14489/Service/TPSLNotify.aspx?pgid=7';
  
-app.all("'/'", function(req, res) {
+app.all('/', function(req, res) {
     apiProxy.web(req, res, {target: serverOne});
 });
 app.listen(process.env.PORT||3000);
