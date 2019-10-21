11ced901eb0a8ff9c3bf09aafea47c634230b3af
sawan4265 <54359989+sawan4265@users.noreply.github.com>
2019-10-21 17:37:41 +0530

    Add files via upload

--- testbean.js
+++ testbean.js
@@ -2,7 +2,7 @@ var express  = require('express');
 var app      = express();
 var httpProxy = require('http-proxy');
 var apiProxy = httpProxy.createProxyServer();
-var serverOne = 'http://220.227.2.106:14489/Service/TPSLNotify.aspx?pgid=7&d=1';
+var serverOne = 'http://220.227.2.106:8010/Service/TPSLNotify.aspx?pgid=6&d=1';
  
 app.all("/", function(req, res) {
     console.log('redirecting to Server1');
